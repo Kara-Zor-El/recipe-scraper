14cea4f9a05124d6e8086bbddabf4a739461e75a
Kara-Zor-El <kara.wilson.2005.08@gmail.com>
2024-07-02 23:26:26 -0400

    Feat(Parser): Add parsing for print pages for pinch of yum

--- parser/pinch_of_yum.go
+++ parser/pinch_of_yum.go
@@ -15,9 +15,40 @@ func (p *PinchOfYumParser) ParseRecipe(e *colly.HTMLElement) shared.Recipe {
 	// categories | capital words of the title split by space
 	// instructions | div.tasty-recipes-instructions
 	// link | current page
-	title := e.ChildText("h2.tasty-recipes-title")
+	link := e.Request.URL.String()
+	// if link contains /print we will handle it differently
+	if strings.Contains(link, "/print") {
+		return p.ParsePrintRecipe(e)
+	} else {
+		title := e.ChildText("h2.tasty-recipes-title")
+		ingredients := []string{}
+		e.ForEach("li[data-tr-ingredient-checkbox]", func(_ int, elem *colly.HTMLElement) {
+			ingredient := elem.Text
+			ingredients = append(ingredients, ingredient)
+		})
+		totalTime := e.ChildText("span.tasty-recipes-total-time")
+		categories := strings.Split(title, " ")
+		var instructions []string
+		e.ForEach("div.tasty-recipes-instructions ol li", func(_ int, elem *colly.HTMLElement) {
+			// append text and urls for images
+			instructions = append(instructions, elem.Text)
+		})
+
+		return shared.Recipe{
+			Title:        title,
+			Ingredients:  ingredients,
+			TotalTime:    totalTime,
+			Categories:   categories,
+			Instructions: instructions,
+			Link:         link,
+		}
+	}
+}
+
+func (p *PinchOfYumParser) ParsePrintRecipe(e *colly.HTMLElement) shared.Recipe {
+	title := e.ChildText("h1.tasty-recipes-title")
 	ingredients := []string{}
-	e.ForEach("li[data-tr-ingredient-checkbox]", func(_ int, elem *colly.HTMLElement) {
+	e.ForEach("ul li", func(_ int, elem *colly.HTMLElement) {
 		ingredient := elem.Text
 		ingredients = append(ingredients, ingredient)
 	})
@@ -28,7 +59,6 @@ func (p *PinchOfYumParser) ParseRecipe(e *colly.HTMLElement) shared.Recipe {
 		// append text and urls for images
 		instructions = append(instructions, elem.Text)
 	})
-
 	return shared.Recipe{
 		Title:        title,
 		Ingredients:  ingredients,
